f5b286e18c60e2fb9561a2498756ff40f61dc0ac
Silvester <silvestaisasi@gmail.com>
2024-01-10 18:21:13 +0300

    Configured cors in server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const auth = require("./routes/auth.js");
 /************ MIDDLEWARES ***********************/
 
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json()); // parse incoming POST/PUT req.body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -17,7 +19,7 @@ app.set("views", "views");
 app.use(express.static(__dirname + "/public"));
 
 /************ ROUTES ***************************/
-// app.options("*", cors());
+app.options("*", cors());
 app.use("/api", auth);
 
 /******* app LISTENING ********/
