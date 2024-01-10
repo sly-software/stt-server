1a35168699f1677426efa5aa904e4bf9ee2ea636
Silvester <silvestaisasi@gmail.com>
2024-01-10 19:05:07 +0300

    Configured cors in server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const auth = require("./routes/auth.js");
 /************ MIDDLEWARES ***********************/
 
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5172", "http://localhost:5173", "https://stt-hfwz.onrender.com"]
+}));
 app.use(express.json()); // parse incoming POST/PUT req.body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
