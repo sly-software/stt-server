5f7c6b1ef995b76c064e23a51b6927c46faa494a
Silvester <silvestaisasi@gmail.com>
2024-01-17 18:40:41 +0300

    removed redundant dist forlder

--- server.js
+++ server.js
@@ -20,14 +20,6 @@ app.use(express.static(__dirname + "/public"));
 /************ ROUTES ***************************/
 app.use("/api", auth);
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "dist/index.html"), function (err) {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-});
-
 /******* app LISTENING ********/
 app.listen(PORT, () => {
   console.log(`App started on PORT: ${PORT}`);
